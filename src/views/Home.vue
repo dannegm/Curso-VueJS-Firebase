0cd5ed3e5786a7a4ad01537e6cbc985e8bdefe37
dannegm <dannegm@gmail.com>
2019-06-03 14:37:59 -0500

    Creando nuestro primer componente

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
 
-    <div class="typer">
-      <h1>Type your <em>Hello</em> message:</h1>
-      <input v-model="helloMessage" />
+        <Typer @input="onInput" />
+        <HelloWorld :msg="helloMessage"/>
     </div>
-
-    <HelloWorld :msg="helloMessage"/>
-  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Typer from '@/components/Typer.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      helloMessage: 'Welcome to Your Vue.js App',
+    name: 'home',
+    components: {
+        HelloWorld,
+        Typer,
+    },
+    methods: {
+        onInput (value) {
+            this.helloMessage = value
+        }
+    },
+    data () {
+        return {
+            helloMessage: 'Welcome to Your Vue.js App',
+        }
     }
-  }
 }
 </script>
