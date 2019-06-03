f266848fd2f42f856dc78327b14b82883db1c13f
dannegm <dannegm@gmail.com>
2019-06-03 16:18:49 -0500

    Creando un componente contenedor

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,56 @@
 <template>
     <div class="app">
-
+        <ThemeController :is-dark="isDark">
+            <div class="hello">
+                <h1>Hola mundo :)</h1>
+                <button @click="toggleTheme">Alternar tema</button>
+            </div>
+        </ThemeController>
     </div>
 </template>
+<script>
+import ThemeController from '@/shared/layouts/ThemeController'
+export default {
+    name: 'App',
+    components: {
+        ThemeController
+    },
+    methods: {
+        toggleTheme () {
+            this.isDark = !this.isDark
+        }
+    },
+    data () {
+        return {
+            isDark: false,
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.app {
+    .hello {
+        display: flex;
+        height: 100vh;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        h1 {
+            font-size: 2em;
+            margin: .5em;
+        }
+        button {
+            display: block;
+            margin: .5em;
+            font-size: 1em;
+            padding: .6em 1em;
+            outline: 0;
+            border: 0;
+            border-radius: 2em;
+            cursor: pointer;
+        }
+    }
+}
+</style>
---
+++ src/shared/layouts/ThemeController.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="theme-container" :class="isDark ? 'dark' : 'clear'">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ThemeController',
+    props: {
+        isDark: {
+            type: Boolean,
+            default: false,
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+$blue-gray-50: #ECEFF1;
+$blue-gray-900: #263238;
+
+.theme-container {
+    display: block;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    transition: background .2s, color .1s;
+
+    * {
+        transition: background .2s, color .1s;
+    }
+
+    &.clear {
+        background: $blue-gray-50;
+        color: $blue-gray-900;
+
+        button {
+            background: #0288D1;
+            color: #fff;
+        }
+    }
+
+    &.dark {
+        background: $blue-gray-900;
+        color: $blue-gray-50;
+
+        button {
+            background: #FFEB3B;
+            color: #000;
+        }
+    }
+}
+</style>
+
