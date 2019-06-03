d51b55ae19f5058a44851b304b7b140e314fbdf4
dannegm <dannegm@gmail.com>
2019-06-03 14:59:29 -0500

    Crear estructura de carpetas del proyecto

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './state/router'
+import store from './state/store'
 
 import '@fortawesome/fontawesome-free'
 import 'bulma'
