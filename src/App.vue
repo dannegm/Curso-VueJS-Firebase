841b0d7b397a6c9c1909d663d3d7901a187d5154
dannegm <dannegm@gmail.com>
2019-06-10 11:08:52 -0500

    Encapsulando NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,19 @@
 <template>
     <div class="app">
         <ThemeController :is-dark="true">
-            <router-link name="{ name: 'timeline' }">Timeline</router-link>
-            <router-link name="{ name: 'login' }">Login</router-link>
-
-
-
-            <nav class="navbar" role="navigation" aria-label="main navigation">
-                <div class="navbar-brand">
-                    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    </a>
-                </div>
-
-                <div id="navbarBasicExample" class="navbar-menu">
-                    <div class="navbar-start">
-                        <a @click="linkTo({ name: 'timeline' })" class="navbar-item">
-                            Timeline
-                        </a>
-                        <a @click="linkTo({ name: 'login' })" class="navbar-item">
-                            Login
-                        </a>
-                    </div>
-                </div>
-            </nav>
-
-
-
+            <NavBar />
             <router-view />
         </ThemeController>
     </div>
 </template>
 <script>
 import ThemeController from '@/shared/layouts/ThemeController'
+import NavBar from '@/shared/layouts/NavBar'
 export default {
     name: 'App',
     components: {
-        ThemeController
-    },
-    methods: {
-        linkTo (route) {
-            this.$router.push(route)
-        },
-        toggleTheme () {
-            this.isDark = !this.isDark
-        }
+        ThemeController,
+        NavBar,
     },
 }
 </script>
-
-<style lang="scss">
-.app {
-}
-</style>
---
+++ src/shared/layouts/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+                <a @click="linkTo({ name: 'timeline' })" class="navbar-item">
+                    Timeline
+                </a>
+                <a @click="linkTo({ name: 'login' })" class="navbar-item">
+                    Login
+                </a>
+            </div>
+        </div>
+    </nav>
+</template>
+<script>
+export default {
+    name: 'NavBar',
+    methods: {
+        linkTo (route) {
+            this.$router.push(route)
+        },
+    },
+}
+</script>
